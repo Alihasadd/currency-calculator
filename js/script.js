b9dfb4765df13856884020a20203e8cb08b8a834
Ali Vidler <alisonvidler@gmail.com>
2024-04-05 12:45:27 -0400

    Currency calculator works

--- js/script.js
+++ js/script.js
@@ -7,6 +7,8 @@ const amountE1_one = document.getElementById('amount-one');
 const amountE1_two = document.getElementById('amount-two');
 const rateE1 = document.getElementById('rate');
 const swap = document.getElementById('swap');
+const opt1 = document.getElementById('currency-opt1');
+const opt2 = document.getElementById('currency-opt2');
 
 function getExchange(event) { 
     const currency_one = currencyE1_one.value;
@@ -21,8 +23,23 @@ function getExchange(event) {
         amountE1_two.value = Math.floor((amountE1_one.value*rate)*1000)/1000
         });
     }
+/*
+HTML.addeventlistener('event', handler(){
+    tell currency1 one to swap places with currency 2
 
-function getExchange2(event) { 
+});
+*/
+console.log(currencyE1_one)
+
+swap.addEventListener('click', function(){
+    let tempValue2 = currencyE1_one.value //holding value1
+    currencyE1_one.value = currencyE1_two.value; //swap 1>2
+    currencyE1_two.value = tempValue2;//swaps 2>1
+    getExchange()
+})
+
+
+/*function getExchange2(event) { 
         const currency_one = currencyE1_one.value;
         const currency_two = currencyE1_two.value;
         console.log(event);
@@ -35,10 +52,11 @@ function getExchange2(event) {
     
             });
         }
-
+*/
 currencyE1_one.addEventListener('change', getExchange); 
-currencyE1_two.addEventListener('change', getExchange2);
+currencyE1_two.addEventListener('change', getExchange);
 amountE1_one.addEventListener('input', getExchange);
-amountE1_two.addEventListener('input', getExchange2);
+amountE1_two.addEventListener('input', getExchange);
+
 
 getExchange();// Is this hoisted
